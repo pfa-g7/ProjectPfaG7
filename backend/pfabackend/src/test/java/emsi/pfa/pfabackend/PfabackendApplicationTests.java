ef0291278be92961b6c38d2a79528aafc2fbdaea
youssefelyou <youssefelyourizi36@gmail.com>
2023-04-07 15:31:34 +0000

    first commit

---
+++ backend/pfabackend/src/test/java/emsi/pfa/pfabackend/PfabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package emsi.pfa.pfabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfabackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
