555225146e2e4e044a81f163c60dd81234efa1a5
Monaim <monaimer99@gmail.com>
2023-04-24 15:56:24 +0100

    signin

--- backend/pfabackend/src/main/java/emsi/pfa/pfabackend/repository/UserRepository.java
+++ backend/pfabackend/src/main/java/emsi/pfa/pfabackend/repository/UserRepository.java
@@ -4,7 +4,10 @@ import emsi.pfa.pfabackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findUserByUsername(String username);
+    List<User> findAll();
 }
