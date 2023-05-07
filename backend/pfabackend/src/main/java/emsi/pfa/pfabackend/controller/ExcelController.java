87b1969f61adb7bed6ba3b2cf35dbad04fd5260b
aimrane <aimrane.essakhi@gmail.com>
2023-05-07 01:20:26 +0100

    upload excel v1

---
+++ backend/pfabackend/src/main/java/emsi/pfa/pfabackend/controller/ExcelController.java
@@ -0,0 +1,40 @@
+package emsi.pfa.pfabackend.controller;
+
+import java.util.List;
+
+import emsi.pfa.pfabackend.entity.ResponseMessage;
+import emsi.pfa.pfabackend.helper.ExcelHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("/api/excel")
+public class ExcelController {
+
+    @PostMapping("/upload")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+        String message = "";
+
+        if (ExcelHelper.hasExcelFormat(file)) {
+            try {
+
+                message = "Uploaded the file successfully: " + file.getOriginalFilename();
+                return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+            } catch (Exception e) {
+                message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+                return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+            }
+        }
+
+        message = "Please upload an excel file!";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(message));
+    }
+
+}
