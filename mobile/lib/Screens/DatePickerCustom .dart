92442a58385d44c640746f5d7145843bfda2b81e
pc <pc@DESKTOP-33IKG22>
2023-05-21 11:26:51 +0100

    date picker and cards

--- mobile/lib/Screens/DatePickerCustom .dart
+++ mobile/lib/Screens/DatePickerCustom .dart
@@ -20,97 +20,76 @@ class _DatePickerCustomState extends State<DatePickerCustom> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        toolbarHeight: 148.0,
-        title: Column(
-          children: [
-            Row(
-              children: const [
-                Icon(
-                  Icons.arrow_back_ios,
-                  color: Colors.orange,
-                ),
-                Expanded(
-                    child: Text("exams",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.black,
-                        )))
-              ],
-            ),
-            const SizedBox(height: 16.0),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              physics: const ClampingScrollPhysics(),
-              child: Row(
-                children: List.generate(
-                  lastDayOfMonth.day,
-                  (index) {
-                    final currentDate =
-                        lastDayOfMonth.add(Duration(days: index + 1));
-                    final dayName = DateFormat('E').format(currentDate);
-                    return Padding(
-                      padding: EdgeInsets.only(
-                          left: index == 0 ? 16.0 : 0.0, right: 16.0),
-                      child: GestureDetector(
-                        onTap: () => setState(() {
-                          selectedIndex = index;
-                        }),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              height: 42.0,
-                              width: 42.0,
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                color: selectedIndex == index
-                                    ? Colors.orange
-                                    : Colors.transparent,
-                                borderRadius: BorderRadius.circular(44.0),
-                              ),
-                              child: Text(
-                                dayName.substring(0, 1),
-                                style: TextStyle(
-                                  fontSize: 24.0,
-                                  color: selectedIndex == index
-                                      ? Colors.white
-                                      : Colors.black54,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 8.0),
-                            Text(
-                              "${index + 1}",
-                              style: const TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.black54,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(height: 8.0),
-                            Container(
-                              height: 2.0,
-                              width: 28.0,
+    return Column(
+      children: [
+        const SizedBox(height: 16.0),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          physics: const ClampingScrollPhysics(),
+          child: Row(
+            children: List.generate(
+              lastDayOfMonth.day,
+              (index) {
+                final currentDate =
+                    lastDayOfMonth.add(Duration(days: index + 1));
+                final dayName = DateFormat('E').format(currentDate);
+                return Padding(
+                  padding: EdgeInsets.only(
+                      left: index == 0 ? 16.0 : 0.0, right: 16.0),
+                  child: GestureDetector(
+                    onTap: () => setState(() {
+                      selectedIndex = index;
+                    }),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 42.0,
+                          width: 42.0,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: selectedIndex == index
+                                ? Colors.orange
+                                : Colors.transparent,
+                            borderRadius: BorderRadius.circular(44.0),
+                          ),
+                          child: Text(
+                            dayName.substring(0, 1),
+                            style: TextStyle(
+                              fontSize: 24.0,
                               color: selectedIndex == index
-                                  ? Colors.orange
-                                  : Colors.transparent,
+                                  ? Colors.white
+                                  : Colors.black54,
+                              fontWeight: FontWeight.bold,
                             ),
-                          ],
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                ),
-              ),
+                        const SizedBox(height: 8.0),
+                        Text(
+                          "${index + 1}",
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.black54,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8.0),
+                        Container(
+                          height: 2.0,
+                          width: 28.0,
+                          color: selectedIndex == index
+                              ? Colors.orange
+                              : Colors.transparent,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
