fd1ce2516895523486b633319bf5e6e617ed0424
aimrane <aimrane.essakhi@gmail.com>
2023-05-23 11:18:42 +0100

    update list exam

--- mobile/lib/Screens/home_page.dart
+++ mobile/lib/Screens/home_page.dart
@@ -4,10 +4,8 @@ import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:get/get.dart';
 import 'package:mobile/model/exam.dart';
 import 'package:mobile/service/exam_service.dart';
-import 'package:mobile/utilities/decoration.dart';
 import 'package:mobile/widgets/loading_indicator.dart';
 import 'package:mobile/widgets/bottom_navigation_bar_widget.dart';
-import 'package:mobile/widgets/no_data_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -141,28 +139,293 @@ class _HomePageState extends State<HomePage> {
                     ],
                   ),
                 ),
-                _buildCalendar(),
-                const Divider(),
-                Container(
-                  height: MediaQuery.of(context).size.height,
-                  decoration: IBoxDecoration.upperBoxDecoration(),
-                  child: FutureBuilder(
-                      future: ExamService.getDataByDate(_selectedDate),
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          return snapshot.data?.length == 0
-                              ? const NoDataWidget()
-                              : Padding(
-                                  padding: const EdgeInsets.only(
-                                      top: 0.0, left: 8, right: 8),
-                                  child: _buildCard(snapshot.data));
-                        } else if (snapshot.hasError) {
-                          return const NoDataWidget();
-                        } else {
-                          return const LoadingIndicatorWidget();
-                        }
-                      }),
-                ),
+                Center(
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        decoration: const BoxDecoration(
+                            color: Color(0xFFD4E7FE),
+                            gradient: LinearGradient(
+                                colors: [
+                                  Color(0xFFF0F0F0),
+                                  Color(0xFFF0F0F0),
+                                ],
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                stops: [0.6, 0.3])),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 20),
+                        child: Column(
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Hi, ${user?.email}",
+                                      style: const TextStyle(
+                                        fontSize: 19,
+                                        fontWeight: FontWeight.w900,
+                                        color:
+                                            Color.fromARGB(255, 161, 161, 161),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      _buildCalendar(),
+                      const Divider(),
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 15),
+                        padding: const EdgeInsets.all(10),
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFF9F9FB),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Text(
+                                  "08:00",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "AM",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              height: 100,
+                              width: 1,
+                              color: Colors.grey.withOpacity(0.5),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width - 160,
+                                  child: const Text(
+                                    "Module 1",
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    // Navigator.push(
+                                    //   context,
+                                    //   MaterialPageRoute(
+                                    //     builder: (context) => ScanningWidget(),
+                                    //   ),
+                                    // );
+                                  },
+                                  child: Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.location_on,
+                                        color: Colors.grey,
+                                        size: 20,
+                                      ),
+                                      const SizedBox(
+                                        width: 5,
+                                      ),
+                                      SizedBox(
+                                        width:
+                                            MediaQuery.of(context).size.width -
+                                                160,
+                                        child: const Text(
+                                          "salle 7",
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                              color: Colors.grey, fontSize: 13),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    // Action à effectuer lorsque la deuxième Row est cliquée
+                                  },
+                                  child: Row(
+                                    children: const [
+                                      CircleAvatar(
+                                        backgroundImage: NetworkImage(
+                                          "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=200&q=80",
+                                        ),
+                                        radius: 10,
+                                      ),
+                                      SizedBox(
+                                        width: 5,
+                                      ),
+                                      Text(
+                                        "etage 2",
+                                        style: TextStyle(
+                                            color: Colors.grey, fontSize: 13),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+
+                            /***box start */
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 15),
+                        padding: const EdgeInsets.all(10),
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFF9F9FB),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Text(
+                                  "10:00",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "AM",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              height: 100,
+                              width: 1,
+                              color: Colors.grey.withOpacity(0.5),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width - 160,
+                                  child: const Text(
+                                    "Module 1",
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    // Navigator.push(
+                                    //   context,
+                                    //   MaterialPageRoute(
+                                    //     builder: (context) => ScanningWidget(),
+                                    //   ),
+                                    // );
+                                  },
+                                  child: Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.location_on,
+                                        color: Colors.grey,
+                                        size: 20,
+                                      ),
+                                      const SizedBox(
+                                        width: 5,
+                                      ),
+                                      SizedBox(
+                                        width:
+                                            MediaQuery.of(context).size.width -
+                                                160,
+                                        child: const Text(
+                                          "salle 7",
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                              color: Colors.grey, fontSize: 13),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    // Action à effectuer lorsque la deuxième Row est cliquée
+                                  },
+                                  child: Row(
+                                    children: const [
+                                      CircleAvatar(
+                                        backgroundImage: NetworkImage(
+                                          "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=200&q=80",
+                                        ),
+                                        radius: 10,
+                                      ),
+                                      SizedBox(
+                                        width: 5,
+                                      ),
+                                      Text(
+                                        "etage 5",
+                                        style: TextStyle(
+                                            color: Colors.grey, fontSize: 13),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+
+                            /***box start */
+                          ],
+                        ),
+                      ),
+                      /*start scan button bar */
+
+                      /*end scan button bar */
+                    ],
+                  ),
+                )
+                // _buildCalendar(),
+                // const Divider(),
+
+                // Container(
+                //   height: MediaQuery.of(context).size.height,
+                //   decoration: IBoxDecoration.upperBoxDecoration(),
+                //   child: FutureBuilder(
+                //       future: ExamService.getDataByDate(_selectedDate),
+                //       builder: (context, snapshot) {
+                //         if (snapshot.hasData) {
+                //           return snapshot.data?.length == 0
+                //               ? const NoDataWidget()
+                //               : Padding(
+                //                   padding: const EdgeInsets.only(
+                //                       top: 0.0, left: 8, right: 8),
+                //                   child: _buildCard(snapshot.data));
+                //         } else if (snapshot.hasError) {
+                //           return const NoDataWidget();
+                //         } else {
+                //           return const LoadingIndicatorWidget();
+                //         }
+                //       }),
+                // ),
               ],
             ),
     );
@@ -210,7 +473,7 @@ class _HomePageState extends State<HomePage> {
                     SizedBox(
                       width: MediaQuery.of(context).size.width - 160,
                       child: const Text(
-                        "Module : ",
+                        "Module 2",
                         overflow: TextOverflow.ellipsis,
                       ),
                     ),
@@ -290,7 +553,7 @@ class _HomePageState extends State<HomePage> {
           _selectedDate =
               "${dateParse.year}-${dateParse.month}-${dateParse.day}";
 
-          _reCallMethodes(_selectedDate);
+          // _reCallMethodes(_selectedDate);
         });
       },
     );
